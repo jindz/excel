07d429c16cb9f9e676f8f89d1c0996c92e590706
i306243089 <yp_306243089@qq.com>
2018-01-04 16:43:32 +0800

    init project

---
+++ src/main/java/com/jindz/excel/App.java
@@ -0,0 +1,13 @@
+package com.jindz.excel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
