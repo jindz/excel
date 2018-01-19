67f63942cc0e2521180cd084165bcf121ddb2817
'i306243089' <yp_306243089@qq.com>
2018-01-19 16:28:58 +0800

    w

--- src/main/java/com/jindz/excel/anno/BaseVo.java
+++ src/main/java/com/jindz/excel/anno/BaseVo.java
@@ -2,8 +2,8 @@ package com.jindz.excel.anno;
 
 public class BaseVo {
 
-	// 小标从0开始，默认排除表头从第二行开始读取
-	private Integer startRow = 2;
+	// 下标从0开始，默认排除表头从第二行开始读取
+	private Integer startRow = 1;
 
 	public Integer getStartRow() {
 		return startRow;
