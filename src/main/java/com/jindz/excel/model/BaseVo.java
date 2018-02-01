e38dbacaba25d1137392d6ca2d70be2fe45c0740
'i306243089' <yp_306243089@qq.com>
2018-02-01 15:28:21 +0800

    jk

---
+++ src/main/java/com/jindz/excel/model/BaseVo.java
@@ -0,0 +1,15 @@
+package com.jindz.excel.model;
+
+public class BaseVo {
+
+	// 下标从0开始，默认排除表头从第二行开始读取
+	private Integer startRow = 1;
+
+	public Integer getStartRow() {
+		return startRow;
+	}
+
+	public Integer getEndRow() {
+		return startRow + 10;
+	}
+}
---
+++ src/main/java/com/jindz/excel/model/Border.java
@@ -0,0 +1,87 @@
+package com.jindz.excel.model;
+
+public class Border {
+	/**
+	 * No border
+	 */
+
+	public final static short BORDER_NONE = 0x0;
+
+	/**
+	 * Thin border
+	 */
+
+	public final static short BORDER_THIN = 0x1;
+
+	/**
+	 * Medium border
+	 */
+
+	public final static short BORDER_MEDIUM = 0x2;
+
+	/**
+	 * dash border
+	 */
+
+	public final static short BORDER_DASHED = 0x3;
+
+	/**
+	 * dot border
+	 */
+
+	public final static short BORDER_HAIR = 0x4;
+
+	/**
+	 * Thick border
+	 */
+
+	public final static short BORDER_THICK = 0x5;
+
+	/**
+	 * double-line border
+	 */
+
+	public final static short BORDER_DOUBLE = 0x6;
+
+	/**
+	 * hair-line border
+	 */
+
+	public final static short BORDER_DOTTED = 0x7;
+
+	/**
+	 * Medium dashed border
+	 */
+
+	public final static short BORDER_MEDIUM_DASHED = 0x8;
+
+	/**
+	 * dash-dot border
+	 */
+
+	public final static short BORDER_DASH_DOT = 0x9;
+
+	/**
+	 * medium dash-dot border
+	 */
+
+	public final static short BORDER_MEDIUM_DASH_DOT = 0xA;
+
+	/**
+	 * dash-dot-dot border
+	 */
+
+	public final static short BORDER_DASH_DOT_DOT = 0xB;
+
+	/**
+	 * medium dash-dot-dot border
+	 */
+
+	public final static short BORDER_MEDIUM_DASH_DOT_DOT = 0xC;
+
+	/**
+	 * slanted dash-dot border
+	 */
+
+	public final static short BORDER_SLANTED_DASH_DOT = 0xD;
+}
